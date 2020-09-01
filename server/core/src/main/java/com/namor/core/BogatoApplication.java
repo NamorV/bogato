4e7e1275828b753174a8736f13764ae0efa05c7b
NamorV <drednout309@gmail.com>
2020-09-01 19:51:35 +0200

    initialized Spring App

---
+++ server/core/src/main/java/com/namor/core/BogatoApplication.java
@@ -0,0 +1,13 @@
+package com.namor.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BogatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BogatoApplication.class, args);
+	}
+
+}
